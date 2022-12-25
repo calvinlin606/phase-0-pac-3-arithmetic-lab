13b0677a0e4b68a1682f4e95fab563d75756f488
Calvin <calvinlin6@gmail.com>
2022-12-24 22:55:49 -0500

    commit tree d613403bede133fdba37e1d17bd6713405ca291e

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract() {
+    return "something";
+  }
+
+  function subtract(a, b) {
+    return "something";
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return "a * b";
+  }
+
+  function multiply(a, b) {
+    return "a * b";
+  }
+
+  function multiply(a, b) {
+    return "a * b";
+  }
+
+  function multiply(a, b) {
+    return "a * b";
+  }
